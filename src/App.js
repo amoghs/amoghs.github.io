c70ef6c0fed4f140d5599b3754e05e476cd1ad49
Amogh Sarda <amogh.sarda@gmail.com>
2019-10-24 16:37:10 +0400

    v1 website complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import hero from "./assets/hero.png";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We'll be back up soon. Hit me on LinkedIn (Amogh Sarda) till then.
-        </p>
-      </header>
-    </div>
+    <Root>
+      <Header>
+        <Link href={"https://www.linkedin.com/in/amoghsarda/"}>LinkedIn</Link>
+        <Link href={"https://medium.com/@amogh.sarda"}>Medium</Link>
+        <Link href={"https://www.instagram.com/studio.amogh/"}>Instagram</Link>
+        <Link href={"https://twitter.com/amoghito"}>Twitter</Link>
+      </Header>
+      <Main>
+        <Hero src={hero} />
+        <Intro>
+          <Line>Hey! I’m Amogh Sarda.</Line>
+          <Line>
+            I’m a product manager who has worked for Atlassian and Intercom. I’m
+            currently working on{" "}
+            <InlineLink href="https://chrome.google.com/webstore/detail/obair/jffaiidojfhflballoapgofphkadiono">
+              Obair
+            </InlineLink>
+            , <InlineLink href="https://www.arenahq.io/">Arena</InlineLink>.
+          </Line>
+          <Line>I love improv comedy, surfing and my uke.</Line>
+        </Intro>
+      </Main>
+    </Root>
   );
 }
 
+const InlineLink = styled.a`
+  color: black;
+`;
+const Root = styled.div`
+  position: absolute;
+  width: 100%;
+  min-height: 100vh;
+  height: 100vh;
+  top: 0;
+  overflow-x: hidden;
+`;
+
+const Header = styled.div`
+  text-align: right;
+  padding: 1rem;
+`;
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const Link = styled.a`
+  padding: 1rem;
+  font-size: 1.5rem;
+  color: black;
+`;
+
+const Intro = styled.div`
+  font-size: 2rem;
+  padding: 1.5rem;
+`;
+
+const Hero = styled.img`
+  max-width: 80vh;
+  padding: 3rem;
+`;
+
+const Line = styled.p``;
 export default App;
